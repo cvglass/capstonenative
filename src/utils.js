71d6b8d61e92dee6a2f77f87ec2330b825bb7216
Rob Ruder <rob@ruders.org>
2017-03-21 11:14:47 -0500

    added end round

--- src/utils.js
+++ src/utils.js
@@ -9,6 +9,7 @@ export const newGuess = 'new guess';
 export const selectPhrase = 'select phrase';
 export const newUser = 'new user';
 export const sendStartGame = 'send start game';
+export const nextDrawing = 'next drawing';
 
 //receive message constants
 export const writeCaption = 'start caption'; // NOTE: startCaption on server-side
@@ -17,6 +18,7 @@ export const startGame = 'start game';
 export const gameOver = 'game over';
 export const receiveRandomPhrase = 'receive random phrase';
 export const youAreTheArtist = 'you are the artist';
+export const scoreboard = 'scoreboard';
 
 //button press emit function
 export const emitToSocket = (emitMsg, emitObj) => {
